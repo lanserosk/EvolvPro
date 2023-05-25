91bd589d36cf69c741de1e20d38e35bfb9de3459
lanserosk <ivan.osoriotobar@gmail.com>
2023-05-25 07:26:59 -0600

    Medio crud

--- EvolvPro/Controllers/HomeController.cs
+++ EvolvPro/Controllers/HomeController.cs
@@ -31,6 +31,16 @@ namespace EvolvPro.Controllers
         {
             return View();
         }
+        public IActionResult DetEstado()
+        {
+            return View();
+        }
+
+        public IActionResult Usuarios()
+        {
+            ViewBag.sesion = HttpContext.Session.GetString("VarSesion1");
+            return View();
+        }
         public IActionResult Menu()
         {
             ViewBag.sesion = HttpContext.Session.GetString("VarSesion1");
@@ -67,6 +77,155 @@ namespace EvolvPro.Controllers
             TempData["Resultado"] = "Intente de nuevo";
             return View();
         }
+
+        [HttpPost]
+        public IActionResult mostrarDestalleEst()
+        {
+            EvolvProContext contexto = new EvolvProContext();
+            var query = from detalleEstado in contexto.DetalleEstados
+                        join estado in contexto.Estados on detalleEstado.FkEstado equals estado.IdEstado
+                        select new
+                        {
+                            detalleEstado.IdDetalleestado,
+                            detalleEstado.ValorDestado,
+                            estado.Nombre
+                        };
+            List<Object> resultado = new List<object>();
+            foreach (var item in query)
+            {
+                resultado.Add(item);
+            }
+            return Json(resultado);
+        }
+
+        //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        //MANTENIMIENTO USUARIOS
+        [HttpPost]
+        public IActionResult mostrarUsuarios()
+        {
+            EvolvProContext contexto = new EvolvProContext();
+            var query = from usu in contexto.Usuarios
+                        join tusu in contexto.TipoUsuarios on usu.FkTipousu equals tusu.IdTipousu
+                        join preUsu in contexto.PreguntaSeguridads on usu.FkPregunta equals preUsu.IdPregunta
+                        select new
+                        {
+                            idUsuario = usu.IdUsuario,
+                            nombreUsu = usu.NombreUsu,
+                            telefonoUsu = usu.TelefonoUsu,
+                            correoUsu = usu.CorreoUsu,
+                            preguntaUsu = preUsu.DescPregunta,
+                            respuestaUsu = usu.RespuestaUsu,
+                            fkEstadousu = usu.FkEstadousu,
+                            fkTipousu = tusu.NombreTipo
+                        };
+            List<Object> resultado = new List<object>();
+            foreach (var item in query)
+            {
+                resultado.Add(item);
+            }
+            return Json(resultado);
+        }
+        [HttpPost]
+        public ActionResult guardarUsuario(Usuario usu)
+        {
+            EvolvProContext contexto = new EvolvProContext();
+            Usuario obj = new Usuario();
+            obj.NombreUsu = usu.NombreUsu;
+            obj.TelefonoUsu = usu.TelefonoUsu;
+            obj.CorreoUsu = usu.CorreoUsu;
+            obj.ContrasenaUsu = usu.ContrasenaUsu;
+            obj.RespuestaUsu = usu.RespuestaUsu;
+
+            //obj.FkEstadousu = usu.FkEstadousu;
+
+            obj.FkTipousu = usu.FkTipousu;
+            obj.FkPregunta = usu.FkPregunta;
+            //CAPTURAMOS ALGUN POSIBLE ERROR
+            try
+            {
+                if (usu.IdUsuario<=0)
+                {
+                    contexto.Usuarios.Add(obj);
+                    contexto.SaveChanges();
+                }
+                else
+                {
+                    contexto.Usuarios.Update(obj);
+                    contexto.SaveChanges();
+                }
+                
+                return Json(true);
+            }
+            catch (Exception ex)
+            {
+                return Json(false);
+            }
+        }
+        [HttpPost]
+        public IActionResult EliminarUsuario(int id)
+        {
+            try
+            {
+                EvolvProContext contexto = new EvolvProContext();
+                var objDel = contexto.Usuarios.FirstOrDefault(x => x.IdUsuario == id);
+                contexto.Usuarios.Remove(objDel);
+                contexto.SaveChanges();
+                return Json(true);
+            }
+
+            catch (Exception ex)
+            {
+                return Json(false);
+            }
+
+        }
+
+        public IActionResult listPreguntas()
+        {
+            EvolvProContext contexto = new EvolvProContext();
+            List<PreguntaSeguridad> preguntasSeguridad = contexto.PreguntaSeguridads.ToList();
+            return Json(preguntasSeguridad);
+        }
+        public IActionResult listTUsuario()
+        {
+            EvolvProContext contexto = new EvolvProContext();
+            List<TipoUsuario> tipoUsuario = contexto.TipoUsuarios.ToList();
+            return Json(tipoUsuario);
+        }
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        public IActionResult listEstado()
+        {
+            EvolvProContext contexto = new EvolvProContext();
+            List<Estado> estados = contexto.Estados.ToList();
+            return Json(estados);
+        }
+
+        [HttpPost]
+        public ActionResult guardarDetEstado(DetalleEstado det)
+        {
+            EvolvProContext contexto = new EvolvProContext();
+            DetalleEstado obj = new DetalleEstado();
+            obj.ValorDestado = det.ValorDestado;
+
+            obj.FkEstado = det.FkEstado;
+            //CAPTURAMOS ALGUN POSIBLE ERROR
+            try
+            {
+                
+                    contexto.DetalleEstados.Add(obj);
+                    contexto.SaveChanges();
+
+
+                return Json(true);
+            }
+            catch (Exception ex)
+            {
+                return Json(false);
+            }
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
